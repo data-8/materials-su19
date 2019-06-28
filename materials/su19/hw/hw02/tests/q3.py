fcbdbb35a9493501c9f8a890ba5513343a07b91a
Rohan Narain <rohannarain@Rohans-MacBook-Pro-3.local>
2019-06-28 12:04:54 -0700

    su19/hw/hw02.push

---
+++ materials/su19/hw/hw02/tests/q3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> all_different.lower() in {'yes', 'no'}
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/su19/hw/hw02/tests/q6_4.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'q6_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> sales.sort(0)
+          box ID | fruit name | count sold | price per fruit ($)
+          25,274 | apple      | 0          | 0.8
+          26,187 | strawberry | 25         | 0.15
+          43,566 | peach      | 17         | 0.8
+          48,800 | orange     | 35         | 0.6
+          52,357 | strawberry | 102        | 0.25
+          53,686 | kiwi       | 3          | 0.5
+          57,181 | strawberry | 101        | 0.2
+          57,930 | grape      | 355        | 0.06
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
